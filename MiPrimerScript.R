fb61d834d6bb26ed1cac038c8c2a572df8900df9
SaraMoraga2402 <smoraga@utem.cl>
2022-07-01 21:02:19 -0400

    Clase final Excel Big Data

--- MiPrimerScript.R
+++ MiPrimerScript.R
@@ -73,13 +73,74 @@ lista_b <- list()
 for (i in var_list){
   print(i)
     if(i =="ana"){
-    lista_b <- c(lista_b,"juan")
+    lista_b <- c(lista_b,"sara")
  }
 print(lista_b)
+                                                                                                                                                                                                                                                                                                                                                                                                         
+}
+#¿Como muestro los elementos de una lista con for?
+var_list<- list("andres",1,T,"ana","juan",F,"loco","dia bonito")
+
+# forma Nro 1
+lista_b<- list()
+
+for (elemento_de_lista in var_list)
+  print("===== nueva iteración=====")
+ print(elemento_de_lista)
+ if(elemento_de_lista == "ana"){
+  print("voy a cambiar ana por juan")
+ lista_b <- c(lista_b,"juan")
+}else{
+  lista_b <- c(lista_b,elemnto_de_lista)
+ }
+    Sys.sleep(2)
+
+print(lista_b)
+#######################################
+resp_lenght <- lenght(var_list)
+for (posición in 1:resp_lenght){
+    print("====nueva iteración======")
+  print(paste("estamos en la posición: ",posición,"- y el valor es :", var_list[posición]))
+  Sys.sleep(2)
+}  
+var_list <- c(var_list,"==")
+print(var_lista)
+#########################################################################################
+#########################Funciones###########################
+
+
+
+reemplazar<-function(vart_iter,val,reemp){
+  print("init funcion reemplazar")
+  respuesta <- list()
+  for (elemento in vart_iter){
+    print("=========nueva iteracion======")
+    print(paste("elemento:",elemento))
+    if(elemento == val){
+    print(paste("se reemplaza: ",elemento,"por",reemp,""))
+    respuesta <- c(respuesta,reemp)
+    }else{
+      respuesta<- c(respuesta,elemento)
+  }
+}
+Sys.sleep(1)
+print("fin funcion reemplazar")
+return(respuesta)
 }
 
+var_list<- list("andres",1,T,"ana","juan",F,"loco","dia bonito")
+ 
+var_list<- reemplazar(var_list,"loco","cuerdo")
+var_list<- reemplazar(var_list,"ana","sara")
+print(var_list)
+
+
+#################################################################################################################################################
+
+library(readxl)
+Datos_Rmjueves30jun <- read_excel("Datos-Rmjueves30jun.xlsx")
+View(Datos_Rmjueves30jun)
 
 
 
-|
 
